0489903aa295d7d7343b0a59ab6530705b8ccf74
tareco244 <106277810+tareco244@users.noreply.github.com>
2023-09-22 08:17:19 -0300

    22/09

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,45 @@
+alert('tareco244');
+
+ function tocaSomPom(){ document.querySelector('.tecla_pom');
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomClap(){ document.querySelector('.tecla_Clap');
+}
+
+document.querySelector('.tecla_Clap').onclick = tocaSomClap;
+
+function tocaSomTim(){ document.querySelector('.tecla_Tim');
+}
+
+document.querySelector('.tecla_Tim').onclick = tocaTim;
+
+function tocaSomPuff(){ document.querySelector('.tecla_Puff');
+}
+
+document.querySelector('.tecla_Puff').onclick = tocaSomPuff;
+
+function tocaSomSplash(){ document.querySelector('.tecla_Splash');
+}
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+function tocaSomToim(){ document.querySelector('.tecla_toim');
+}
+
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+function tocaSomPsh(){ document.querySelector('.tecla_Psh');
+}
+
+document.querySelector('.tecla_Psh').onclick = tocaSomPsh;
+
+function tocaSomTic(){ document.querySelector('.tecla_Tic');
+}
+
+document.querySelector('.tecla_Tic').onclick = tocaSomTic;
+
+function tocaSomTom(){ document.querySelector('.tecla_Tom');
+}
+
+document.querySelector('.tecla_Tom').onclick = tocaSomTom;
